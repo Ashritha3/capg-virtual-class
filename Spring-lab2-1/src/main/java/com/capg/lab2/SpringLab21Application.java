fa73c7aa3c801a75a9269725259aa91dfc7b4f93
Ashritha <Ashritha@DESKTOP-72O2310>
2020-04-18 21:19:56 +0530

    pushed

---
+++ Spring-lab2-1/src/main/java/com/capg/lab2/SpringLab21Application.java
@@ -0,0 +1,13 @@
+package com.capg.lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLab21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLab21Application.class, args);
+	}
+
+}
