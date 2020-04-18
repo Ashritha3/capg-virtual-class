896993ec9cd114e60f6c794e066e332cbba62491
Ashritha <Ashritha@DESKTOP-72O2310>
2020-04-18 21:52:55 +0530

    push

---
+++ Spring-lab3/src/main/java/com/capg/lab3/SpringLab32Application.java
@@ -0,0 +1,13 @@
+package com.capg.lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLab32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLab32Application.class, args);
+	}
+
+}
---
+++ Spring-lab3/src/test/java/com/capg/lab3/SpringLab32ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLab32ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
